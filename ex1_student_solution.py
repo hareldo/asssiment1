d0a38d994285474806b1621030c56a4fb5956b18
hareld1 <harel.doitch@scopiolabs.com>
2022-12-08 12:19:10 +0200

    Q4

--- ex1_student_solution.py
+++ ex1_student_solution.py
@@ -5,10 +5,8 @@ from typing import Tuple
 from random import sample
 from collections import namedtuple
 
-
 from numpy.linalg import svd
 from scipy.interpolate import griddata
-
 
 PadStruct = namedtuple('PadStruct',
                        ['pad_up', 'pad_down', 'pad_right', 'pad_left'])
@@ -16,6 +14,7 @@ PadStruct = namedtuple('PadStruct',
 
 class Solution:
     """Implement Projective Homography and Panorama Solution."""
+
     def __init__(self):
         pass
 
@@ -67,9 +66,32 @@ class Solution:
         Returns:
             The forward homography of the source image to its destination.
         """
-        # return new_image
-        """INSERT YOUR CODE HERE"""
-        pass
+        h_dst, w_dst = dst_image_shape[:2]
+        h_src, w_src = src_image.shape[:2]
+
+        border_src = np.array([[0, 0, 1], [0, h_src, 1], [w_src, h_src, 1], [w_src, 0, 1]])
+        border_src_in_dst = homography.dot(border_src.T)
+        border_src_in_dst = border_src_in_dst[:2] / border_src_in_dst[-1, :]
+        border_dst = np.array([[0, 0], [0, h_dst], [w_dst, h_dst], [w_dst, 0]])
+        all_borders = np.vstack((border_dst, border_src_in_dst.T))
+        min_cords = np.around(all_borders.min(axis=0)).astype(int)
+        max_cors = np.flip(np.array(dst_image_shape[:2])) - 1
+
+        new_image = np.zeros(dst_image_shape)
+        for v in range(src_image.shape[0]):
+            for u in range(src_image.shape[1]):
+                x = np.array([u, v, 1])
+                x_tag = homography.dot(x)
+                x_tag = (x_tag / x_tag[2])
+                dst_crds = np.around(x_tag[:2] - min_cords).astype(int)
+
+                # Make sure not crossing boundaries
+                dst_crds = np.minimum(dst_crds, max_cors)
+                dst_crds = np.maximum(dst_crds, np.array([0, 0]))
+
+                new_image[dst_crds[1], dst_crds[0], :] = src_image[v, u, :] / 255
+
+        return new_image
 
     @staticmethod
     def compute_forward_homography_fast(
